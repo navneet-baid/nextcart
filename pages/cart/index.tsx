6c4bb03f56f77f98a1e3385ba58003be995ad2bd
navneet-baid-t2g <navneet.baid@tech2globe.in>
2025-10-08 02:49:04 +0530

    fix: improve responsiveness on mobile and tablet

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -69,6 +69,7 @@ export default function Cart() {
                                                 alt={item.title}
                                                 width={100}
                                                 height={100}
+                                                style={{ objectFit: "contain" }}
                                                 className="rounded"
                                             />
                                         </div>
